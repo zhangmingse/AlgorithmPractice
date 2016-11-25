52a6575387b690b39940cfccef86a62e9761f32d
zhangming <zhangming.se.cn@gmail.com>
2016-11-25 15:29:55 +0800

    print 12121212.... .this code is beautiful

---
+++ src/concurrency_practice/OutputThread.java
@@ -0,0 +1,40 @@
+package concurrency_practice;
+
+public class OutputThread implements Runnable {
+
+    private int num;
+    private Object lock;
+    
+    public OutputThread(int num, Object lock) {
+        super();
+        this.num = num;
+        this.lock = lock;
+    }
+
+    public void run() {
+        try {
+            while(true){
+                synchronized(lock){
+                    lock.notifyAll();
+                    lock.wait();
+                    System.out.println(num);
+                }
+            }
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public static void main(String[] args){
+        final Object lock = new Object();
+        
+        Thread thread1 = new Thread(new OutputThread(1,lock));
+        Thread thread2 = new Thread(new OutputThread(2, lock));
+        
+        thread1.start();
+        thread2.start();
+    }
+
+}
